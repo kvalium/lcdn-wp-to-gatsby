f201c0046abcfa3e784a68f9632989a0b166439c
Julien Monchany <julien.monchany@gmail.com>
2019-09-10 10:37:30 +0200

    revert the yo dawg test

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,7 +36,6 @@ export default class Index extends React.Component {
     const { displayedPosts } = this.state
     return (
       <Layout>
-        <h1>Yo dawg</h1>
         <div className="columns is-multiline">
           {displayedPosts.map(({ node }) => {
             return (
