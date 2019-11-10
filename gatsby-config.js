5bcea49574c574887d0c645d51a70560e90f8d16
Kvalium <julien.monchany@gmail.com>
2019-11-10 15:50:41 +0100

    test cache invalidation

--- gatsby-config.js
+++ gatsby-config.js
@@ -139,7 +139,7 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+    //`gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
     `gatsby-plugin-netlify-cache`,
   ],
