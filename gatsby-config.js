2a6d43d7d030a97a176f5e63bec51ec0df2baa37
kvalium <jmy@DESKTOP-N4D4JFJ.localdomain>
2020-01-09 17:55:05 +0100

    THAT FUCKING CACHE DOES NOT INVALIDATE

--- gatsby-config.js
+++ gatsby-config.js
@@ -143,16 +143,16 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
-    {
-      resolve: `gatsby-plugin-netlify`,
-      options: {
-        headers: {
-          "/*": [
-            "cache-control: public, max-age=0, must-revalidate",
-          ]
-        }
-      }
-    },
+    // {
+    //   resolve: `gatsby-plugin-netlify`,
+    //   options: {
+    //     headers: {
+    //       "*": [
+    //         "cache-control: public, max-age=0, must-revalidate",
+    //       ]
+    //     }
+    //   }
+    // },
     // `gatsby-plugin-netlify-cache`,
     {
       resolve: `gatsby-plugin-netlify-identity`,
